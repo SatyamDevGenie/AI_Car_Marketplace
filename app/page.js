86f7edfced368416dd08c79951c74546e8f069fe
SatyamDevGenie <satyamsawant54@gmail.com>
2025-10-21 15:04:39 +0530

    Modify -- header.jsx component + Clerk

--- app/page.js
+++ app/page.js
@@ -1,10 +1,8 @@
-import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div>Suscribe to SatyamCoder
-      <br/>
-      <Button variant="destructive">Suscribe</Button>
+    <div>
+     
     </div>
   );
 }
