9ef860d0352851e445486ca3b195da5a5f88571c
SatyamDevGenie <satyamsawant54@gmail.com>
2025-10-21 14:15:52 +0530

    Added Images of Cars & Brands -- public folder

--- components/header.jsx
+++ components/header.jsx
@@ -1,10 +1,9 @@
-import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from '@clerk/nextjs'
 import React from 'react'
 
 const Header = () => {
     return (
         <div>
-           Header
+            Header
         </div>
     )
 }
